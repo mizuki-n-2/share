bfa566ac506e66390d57e561dba21b5d408b35a3
mizuki-n-2 <mizukizhangze@gmail.com>
2020-09-23 19:23:21 +0900

    first commit

---
+++ src/components/Message.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div v-for="(value,index) in shares" :key="index">
+      <div class="message">
+        <div class="flex">
+          <p class="name">{{value.name}}</p>
+          <img class="icon" src="../assets/heart.png" alt="">
+          <p class="number">{{value.like.length}}</p>
+          <img class="icon" src="../assets/cross.png" alt="">
+          <img class="icon detail" src="../assets/detail.png" alt="">
+        </div>
+        <p class="text">{{value.share}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      shares:[
+        {
+          name: "太郎",
+          like: [],
+          share: "初めまして"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+
+.icon {
+  width: 25px;
+  height: 25px;
+}
+
+.detail {
+  margin-left: 50px;
+}
+
+.message {
+  padding: 20px;
+  border-bottom: 1px solid white;
+  border-left: 1px solid white;
+}
+
+.name {
+  font-size: 18px;
+  font-weight: bold;
+  margin-right: 10px;
+}
+
+.text {
+  margin-top: 10px;
+}
+
+.number {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+</style>
---
+++ src/components/SideNavi.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="left-padding">
+      <img class="logo" src="../assets/logo.png" alt="">
+      <div class="flex icon-field">
+        <img class="icon" src="../assets/home.png" alt="">
+        <p>ホーム</p>
+      </div>
+      <div class="flex icon-field">
+        <img class="icon" src="../assets/profile.png" alt="">
+        <p>プロフィール</p>
+      </div>
+      <div class="flex icon-field">
+        <img class="icon" src="../assets/logout.png" alt="">
+        <p>ログアウト</p>
+      </div>
+      <hr />
+      <ShareMessage />
+    </div>
+  </div>
+</template>
+
+<script>
+import ShareMessage from '../components/ShareMessage';
+export default {
+  components: {
+    ShareMessage
+  }
+};
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+
+.left-padding {
+  margin: 20px;
+}
+
+.logo {
+  width: 100px;
+}
+
+.icon-field {
+  margin-top: 15px;
+  cursor: pointer;
+}
+
+.icon-field p {
+  font-size: 16px;
+  padding-left: 15px;
+}
+
+.icon {
+  width: 25px;
+  height: 25px;
+}
+</style>
